a1da140cb06c82fb71b90740e655ee18e237bbad
SeiverB <43014287+SeiverB@users.noreply.github.com>
2022-03-09 15:19:04 -0700

    Fixed un-needed import

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import org.junit.experimental.theories.suppliers.TestedOn;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
